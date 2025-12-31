678d1cf5d1b88a094ed15eef8d9dcbe3066cdf71
Anthony Wu <462072+anthonywu@users.noreply.github.com>
2025-12-30 22:22:21 -0800

    remove noisy print during flow matching

--- src/chatterbox/models/s3gen/flow_matching.py
+++ src/chatterbox/models/s3gen/flow_matching.py
@@ -236,7 +236,6 @@ class CausalConditionalCFM(ConditionalCFM):
         in_dtype = x.dtype
         x, t_span, mu, mask, spks, cond = cast_all(x, t_span, mu, mask, spks, cond, dtype=self.estimator.dtype)
 
-        print("S3 Token -> Mel Inference...")
         for t, r in tqdm(zip(t_span[..., :-1], t_span[..., 1:]), total=t_span.shape[-1] - 1):
             t, r = t[None], r[None]
             dxdt = self.estimator.forward(x, mask=mask, mu=mu, t=t, spks=spks, cond=cond, r=r)
